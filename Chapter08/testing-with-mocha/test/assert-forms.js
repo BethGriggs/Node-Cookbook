5a12408c7823b725bf63b3fd94a9530ba0174f72
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-10-06 01:25:31 +0100

    Chapter 8: Mocha samples update

--- Chapter08/testing-with-mocha/test/assert-forms.js
+++ Chapter08/testing-with-mocha/test/assert-forms.js
@@ -1,14 +1,18 @@
 const assert = require("chai").assert;
+const expect = require("chai").expect;
+require("chai").should();
+
 const calculator = require("../calculator.js");
 
 describe("Calculator Test", () => {
   describe("add() Test", () => {
     it("add(1,2) should return 3", () => {
+      // assert form
       assert.equal(calculator.add(1, 2), 3);
-    });
-
-    it("add('1','2') should return 3", () => {
-      assert.equal(calculator.add("1", "2"), 3);
+      // expect form
+      expect(calculator.add(1, 2)).to.equal(3);
+      // should form
+      calculator.add(1, 2).should.equal(3);
     });
   });
 });
